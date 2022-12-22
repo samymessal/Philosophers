5b56ecf823f6051db79956d7bf9fad2374de8871
samymessal <samy.messal.sm@gmail.com>
2022-12-22 14:07:59 +0100

    init

---
+++ includes/philosophers.h
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   philosophers.h                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: smessal <smessal@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/12/22 12:51:47 by smessal           #+#    #+#             */
+/*   Updated: 2022/12/22 12:52:32 by smessal          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PHILOSOPHERS_H
+#define PHILOSOPHERS_H
+
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <string.h>
+#include <pthread.h>
+
+#endif
