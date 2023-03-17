c38622e803860e5136bf9c0376b4ca2a04a96762
samymessal <samy.messal.sm@gmail.com>
2023-03-17 23:22:08 +0100

    fixed checker, reste helgrind

--- philo/srcs/checker.c
+++ philo/srcs/checker.c
@@ -6,7 +6,7 @@
 /*   By: smessal <smessal@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/12 15:39:50 by smessal           #+#    #+#             */
-/*   Updated: 2023/03/17 22:43:04 by smessal          ###   ########.fr       */
+/*   Updated: 2023/03/17 23:20:59 by smessal          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,13 +20,12 @@ int	dies(t_data *data)
 	time_t	time;
 
 	philo = data->philo;
+	usleep(philo->t_die);
 	while (1 && philo)
 	{
         pthread_mutex_lock(&philo->mut_ate);
 		time = timer();
-		// if (philo->num_philo == 1)
-		// 	return (1);
-		if (philo->ate > 0 && time - philo->ate >= data->t_die)
+		if (time - philo->ate >= data->t_die)
 		{
 			data->philo_died = philo;
 			printf("%ld %s died\n", time - data->time, data->philo_died->val_c);
@@ -36,7 +35,10 @@ int	dies(t_data *data)
         pthread_mutex_unlock(&philo->mut_ate);
         philo = philo->next;
         if (!philo)
+		{	
             philo = data->philo;
+			usleep(philo->t_die);
+		}
 	}
 	return (0);
 }
